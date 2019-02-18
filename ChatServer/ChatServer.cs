8422f57fd04d79ee87634956781be4268ebb3390
marckostelecky <marc.kostelecky@gmail.com>
2019-02-18 16:28:08 -0600

    intial gay asss ass asss asss commit ass

---
+++ ChatServer/ChatServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Photon.SocketServer;
+
+public class ChatServer : ApplicationBase
+{
+    protected override PeerBase CreatePeer(InitRequest initRequest)
+    {
+        return new ChatPeer(initRequest);
+    }
+
+    protected override void Setup()
+    {
+
+    }
+
+    protected override void TearDown()
+    {
+
+    }
+}
